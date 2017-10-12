dc374864911aeef321e1708e43c2da40091fc480
Pelle Pastoor <pelle.pastoor217@gmail.com>
2017-10-12 22:53:00 +0200

    Basic front page
    Some basics of the front page is done.

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Streaming Site</title>
+        
+        <link rel="stylesheet" type="text/css" href="css/menu.css">
+    </head>
+    <body>
+        <div class="menu">
+            
+            <p><img class="menu-logo" src="img/logo.png" width="120" height="50"></p>
+            
+            <p class="menu-search">
+                <form action="search.php">
+                    <input class="menu-search-input" type="search" name="q" placeholder="Search..">
+                    <input class="menu-search-submit" type="submit" value="Search">
+                </form>
+            </p>
+        </div>
+        
+        <div class=""></div>
+    </body>
+</html>
