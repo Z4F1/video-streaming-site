c9cd63b8f7aed0a0c51821b37a724f05db7be404
Pelle Pastoor <pelle.pastoor217@gmail.com>
2017-10-14 00:25:04 +0200

    Pages
    Added pages, not functional. Just looks for now

---
+++ php/template/header.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+        
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link rel="stylesheet" type="text/css" href="css/menu.css">
+        <link rel="stylesheet" type="text/css" href="css/video.css">
+    </head>
+    <body>
+        <div class="menu">
+            
+            <p><img class="menu-logo" src="img/logo.png" width="120" height="50"></p>
+            
+            <p class="menu-search">
+                <form action="search.php">
+                    <input class="menu-search-input" type="search" name="q" placeholder="Search..">
+                    <input class="menu-search-submit" type="submit" value="Search">
+                </form>
+            </p>
+        </div>
---
+++ view.php
@@ -0,0 +1,18 @@
+<?php
+$title = "Streaming Site";
+include "php/template/header.php";
+
+$video_title = "Hei guys";
+?>
+
+<div class="container">
+    <div class="video">
+        <video class="video-screen" src="#" width="800px" height="600px"></video>
+        <h2 class="video-title"><?php echo $video_title; ?></h2>
+    </div>
+</div>
+
+
+<?php
+include "php/template/footer.php";
+?>
